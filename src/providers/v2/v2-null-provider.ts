d94cd933674e093cd22cd88f4f3f046b4251f754
Joshua Dechant <josh@codecrafting.org>
2025-01-28 13:50:49 -0500

    Fix issue with router trying to find v2 pools (#1)
    * Fix issue with router trying to find v2 pools
    
    * lint

---
+++ src/providers/v2/v2-null-provider.ts
@@ -0,0 +1,19 @@
+import { Token } from "@uniswap/sdk-core";
+import { IV2PoolProvider } from "../../contrib/smart-order-router/providers/v2/pool-provider";
+
+export class V2NullProvider implements IV2PoolProvider {
+  async getPools() {
+    return {
+      getPool: () => undefined,
+      getPoolByAddress: () => undefined,
+      getAllPools: () => [],
+    };
+  }
+  getPoolAddress(tokenA: Token, tokenB: Token) {
+    return {
+      poolAddress: "",
+      token0: tokenA,
+      token1: tokenB,
+    };
+  }
+}
